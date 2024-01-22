343cc333006b8c3f4d8765ee1674c54fb60d7fd9
Leonardo Silva <leo.hhcsilva@gmail.com>
2024-01-21 21:29:16 -0300

    Minimax implemented

--- game/player.py
+++ game/player.py
@@ -1,3 +1,4 @@
+import math
 import random
 from time import sleep
 from enum import Enum
@@ -49,9 +50,8 @@ class HumanPlayer(Player):
                 print("The chosen position is occupied. Try again.")
             else:
                 break
-        board.occupy_position(choice, self.repr.name)
+        board.move(choice, self.repr.name)
             
-
 
 # Defines a player that randomly chooses a position to play
 class RandomPlayer(Player):
@@ -63,10 +63,9 @@ class RandomPlayer(Player):
         choice = random.choice(board.get_available_positions())
         
         print(f"It's {self.repr.name}'s turn. {self.repr.name} chooses position {choice}.")
-        sleep(1) # Adds a small delay of 1 second to make the UI clearer; optional
+        sleep(0.5) # Adds a small delay of 0.5 second to make the UI clearer; optional
 
-        board.occupy_position(choice, self.repr.name)
-
+        board.move(choice, self.repr.name)
 
 
 # Defines a player controlled by AI based on the Minimax algorithm
@@ -74,16 +73,71 @@ class ComputerPlayer(Player):
 
     def __init__(self, repr: PlayerRepr) -> None:
         super().__init__(repr)
+        self.is_max_player = True if self.repr == PlayerRepr.X else False
 
     def move(self, board: Board):
-        available_positions = board.get_available_positions()
         
         if board.is_empty(): # Chooses random position if the board is empty
             choice = random.choice(board.get_available_positions())
         else: # Executes the Minimax algorithm
-            choice = self.minimax()
+            choice = self.minimax(
+                board, self.is_max_player, depth=0,
+                alpha=float('-inf'), beta=float('inf'),
+            ).get('position')
 
-        board.occupy_position(choice, self.repr.name)
+        print(f"It's {self.repr.name}'s turn. {self.repr.name} chooses position {choice}.")
+        sleep(0.5) # Adds a small delay of 0.5 second to make the UI clearer; optional
 
-    def minimax(self):
-        pass
+        board.move(choice, self.repr.name)
+
+    # Minimax algorithm with alpha-beta pruning, which runs recursively up to 'depth'
+    def minimax(self, board: Board, is_maximizing: bool, depth: int, alpha: float, beta: float) -> dict:
+        from tictactoe import GameState
+
+        # Check if the game is over or maximum depth has been reached
+        if board.check_winner(PlayerRepr.X.name):
+            return {'position': None, 'score': GameState.X_WINS.value}
+        elif board.check_winner(PlayerRepr.O.name):
+            return {'position': None, 'score': GameState.O_WINS.value}
+        elif board.is_fully_occupied() or depth > 5:
+            return {'position': None, 'score': GameState.TIE.value}
+        
+        # Initial score for min/max player
+        if is_maximizing:
+            best_metrics = {'position': None, 'score': -math.inf}
+        else:
+            best_metrics = {'position': None, 'score': math.inf}
+
+        # Simulate move on all available positions (game state tree):
+        # 1. Execute move on board;
+        # 2. Call minimax() recursively;
+        # 3. Undo move;
+        # 4. Update best metrics;
+        # 5. Do the alpha-beta pruning step.
+        for position in board.get_available_positions():
+
+            board.move(position, PlayerRepr.X.name if is_maximizing else PlayerRepr.O.name)
+
+            metrics = self.minimax(board, not is_maximizing, depth + 1, alpha, beta)
+            metrics['position'] = position
+
+            board.undo_move(position)
+
+            # Update best metrics according to min/max player
+            if is_maximizing:
+                if metrics['score'] > best_metrics['score']:
+                    best_metrics = metrics
+            else:
+                if metrics['score'] < best_metrics['score']:
+                    best_metrics = metrics
+
+            # Alpha-beta pruning
+            if is_maximizing:
+                alpha = max(alpha, best_metrics['score'])
+            else:
+                beta = min(beta, best_metrics['score'])
+
+            if beta <= alpha:
+                break
+
+        return best_metrics
